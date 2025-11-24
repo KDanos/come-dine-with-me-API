fc91845c7d43a114d36a425ec936fa47caf11400
Konstantin <konstantin_danos@yahoo.de>
2025-11-24 15:32:35 +0200

    created the dinner model

---
+++ models/dinners.js
@@ -0,0 +1,26 @@
+import mongoose from 'mongoose'
+
+const commentSchema = new mongoose.Schema ({
+title: {type: String, required: true},
+text: {type: String, required: true},
+owner: {type: mongoose.Schema.Types.ObjectId, ref : 'User'}
+}, 
+{timestamps:true}
+)
+
+const dinnerSchema = new mongoose.Schema ({
+    host: {type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true},
+    theme: {type: String, required: true},
+    starter: {type: String},
+    main: {type: String},
+    dessert: {type: String},
+    drink: {type: String},
+    comments: [commentSchema],
+    guests: {type: [mongoose.Schema.Types.ObjectId], ref: 'User'},
+    isCompleted: {type: Boolean}
+}, 
+{timestamps: true}
+)
+
+const Dinner= mongoose.model('Dinners', dinnerSchema)
+export default Dinner
